0ce0e518797857b40a633c77d38e67ba19a2b0cb
chenBan <627604780@qq.com>
2020-12-30 16:47:02 +0800

    the first commit and executable

---
+++ SIT-EpidemicReport.js
@@ -0,0 +1,128 @@
+const axios = require('axios')
+const md5  = require('js-md5')
+
+const url_historyRepoter = "http://210.35.96.114/report/report/getMyReport"
+const url_todayRepoter = 'http://210.35.96.114/report/report/todayReport'
+
+headers = {
+    "Content-Type": "application/json",
+    "Origin": "http://xgfy.sit.edu.cn",
+    "Host": "210.35.96.114"
+}
+
+// const userCodeArray = ['181042Y102','1810300127','181021Y136','181012Y120','1810300122']
+
+const user = {
+    '181042Y102':'陈鑫',
+    '1810300127':'文琪',
+    '181021Y136':'刘妍',
+    '181012Y120':'赵晓莹',
+    '1810300122' : '宋佳琪'
+}
+
+const genearteMD5 = (val)=>{
+    return md5.hex(val)
+}
+
+const decode = (userCode , t)=>{
+    const prefix = "Unifri"
+    const timeStamp = t
+    let hashed = genearteMD5(userCode + prefix + timeStamp)  
+    return (hashed.slice(16) + hashed.slice(0,16)).toUpperCase()
+}
+
+const initHistoryRepoterRequestDataArray = ()=>{
+    let historyRepoterRequestDataArray = []
+    for(const userCode of Object.keys(user)){
+        let historyRepoterRequestData = {
+            "usercode" : "", 
+            "batchno" : ""
+        }
+        historyRepoterRequestData.usercode = userCode
+        historyRepoterRequestDataArray.push(historyRepoterRequestData)
+    }
+    return historyRepoterRequestDataArray
+}
+
+const getHistoryRepoter = async (historyRepoterRequestDataArray)=>{
+    let historyRepoterConfig = {
+        method: 'post',
+        url: url_historyRepoter,
+        headers,
+    }
+    let yesterdayInfoArray = []
+    
+    const t = new Date().getTime().toString()
+    for (const historyRepoterRequestData of historyRepoterRequestDataArray) {
+        historyRepoterConfig.data = JSON.stringify(historyRepoterRequestData)
+        historyRepoterConfig.headers.decodes = decode(historyRepoterRequestData.usercode , t)
+        historyRepoterConfig.headers.ts = t
+        try{
+            let result = await axios(historyRepoterConfig)
+            console.log(`获取学号为${user[historyRepoterRequestData.usercode]}同学前一天的上报信息成功`)
+            yesterdayInfoArray.push(result.data.data[0])
+        }catch(error){
+            throw new error(`${ user[yesterdayInfo.usercode]  }同学的信息上报出错`)          
+        }
+    }
+    return yesterdayInfoArray
+}
+
+
+const todayReport = async (yesterdayInfoArray)=>{
+    let todayRepoterConfig = {
+        method: 'post',
+        url: url_todayRepoter,
+        headers,
+    }
+    const t = new Date().getTime().toString()
+    const today = new Date().getFullYear().toString()+(new Date().getMonth()+1)+new Date().getDate()
+    
+    for (const yesterdayInfo of yesterdayInfoArray) {
+        console.log(yesterdayInfo.batchno != today?`${ user[yesterdayInfo.usercode]  }今日未上报`:`${ user[yesterdayInfo.usercode]  }今日已上报`)
+        if(yesterdayInfo.batchno != today){
+            delete yesterdayInfo.ksfl2
+            delete yesterdayInfo.jttw2
+            delete yesterdayInfo.id
+            delete yesterdayInfo.currentsituation2
+            delete yesterdayInfo.wendu2
+            delete yesterdayInfo.studentclass
+            delete yesterdayInfo.auditor
+            delete yesterdayInfo.mobile
+            delete yesterdayInfo.remarks
+            delete yesterdayInfo.creattime
+            todayRepoterConfig.data = JSON.stringify(yesterdayInfo)
+            todayRepoterConfig.headers.decodes = decode(yesterdayInfo.usercode , t)
+            todayRepoterConfig.headers.ts = t
+            try {
+                const result = await axios(todayRepoterConfig)   
+                if(result.data.code == 0){
+                    console.log(`${ user[yesterdayInfo.usercode]  }同学的信息上报成功`)
+                }else{
+                    throw new error(`${ user[yesterdayInfo.usercode]  }同学的信息上报出错`)          
+                }        
+            } catch (error) {
+                throw new error(`${ user[yesterdayInfo.usercode]  }同学的信息上报出错`)          
+            }
+            
+        }
+    }
+    
+}
+
+const main = async (n = 1)=>{
+    if(n<=10){
+        try {
+            const historyRepoterRequestDataArray =  initHistoryRepoterRequestDataArray()
+            const yesterdayInfoArray = await getHistoryRepoter(historyRepoterRequestDataArray)
+            await todayReport(yesterdayInfoArray)
+        } catch (error) {
+            console.log(error)
+            await main (++n)
+        }
+    }
+}
+
+setInterval(() => {
+    main()
+}, 1000*60*60*4);
---
+++ youthStudy.js
@@ -0,0 +1,100 @@
+const axios = require('axios')
+const utils = require('util') 
+
+const requset = axios.create({
+    method : "GET",
+    baseURL : "http://qcsh.h5yunban.com/youth-learning/cgi-bin",
+    headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36 QBCore/4.0.1301.400 QQBrowser/9.0.2524.400 Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2875.116 Safari/537.36 NetType/WIFI MicroMessenger/7.0.5 WindowsWechat",
+    }
+})
+
+
+const cardNoList = {
+    'okMqsjjQxqSOR8LukID35aCS5hss' : '陈鑫',
+    'okMqsjhiFeis9b28-rLwHWBHk1bo' : '王智',
+    'okMqsjn2dd2cvGvRYRAsQuTMDPog' : '黄日宇',   
+    'okMqsjqaX8u2o1jEqXr60HGOQNK8' : '宋安邦',
+    'okMqsjiN9bmo8CYEbY8Q4tTeD8Xo' : '王佳龙',
+    'okMqsjl4KQxhIXbw4n43hqVNfR3o' : '彭天琦',
+    'okMqsjoLRaoh4OecONgN6QTh2_MI' : '李鹏鑫',
+    'okMqsjmhjo9WBk52_E64FuzpGkJU' : '王书田',
+    'okMqsjivo8Y96c4HKjuIzvR_uzvE' : '应捷',
+    'okMqsjlrvW2Xfp39m-N-zG9n_Ccs' : '王浩洋',
+    'okMqsjk4OEisYk9aqSNaRh_aU50M' : '贺婧佩',
+    'okMqsjqhiRaA9JWUnaebu9fhZBlE' : '方静',
+    'okMqsjqruQfdIbeg8DItOAUBW2TI' : '王锦华',
+    'okMqsjgiiy0rgYHg3SaNQ-LwOf9I' : '赵培任',
+    'okMqsjiDH1ieOaEPmnFkPPiBRSBU' : '王清',
+    'okMqsjtdye1N_WXHhRWuaGsMB96g' : '刘雨璇',
+    'okMqsjkzRAAwAg5f_4RjJApFlF98' : '刘疆泉',
+    'okMqsjo55TQhGAJeTygh2DMHneVc' : '严如刚',
+    'okMqsjjUTc_zq_mcuMGiuUrdhg-I' : '王彪',
+    'okMqsjqEhbRnnELirhjLlFMaDrJ8' : '衷涛',
+    'okMqsjgqGWpLLfTruPoPX4qadWT4' : '张万垚',
+    'okMqsjq5kZK_nUiIGDtv2zx93eaQ' : '何佳旺'
+}
+console.log(Object.keys(cardNoList).length)
+    
+
+
+
+async function youthStudyRepoter(){
+
+    let currentConfig = {
+        url:`/common-api/course/current`,
+    }
+
+    let result1 = await requset(currentConfig)
+    const courseId = result1.data.result.id
+    console.log(courseId)
+    
+    for (const [key , value] of Object.entries(cardNoList)) {
+        
+        let url = '/login/we-chat/callback?callback=http%3A%2F%2Fqcsh.h5yunban.com%2Fyouth-learning%2Findex.php&scope=snsapi_userinfo&appid=wxa693f4127cc93fad&openid=%s&nickname=%25E9%2599%2588ban&headimg=https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FIydG4iaw1BsK71SHQDI0wPIRHPPicQLPCnMZcut5OHWrGzEYib5z7YEJcia9TpAxHENaSkNCptesggMNU3Tkh0XSaQ%2F132&time=%s&source=common'
+        const time = parseInt(new Date().getTime() / 1000)
+        url = utils.format(url,key,time)
+        let tokenConfig = {
+            url,
+          };
+    
+        let result = await requset(tokenConfig)
+        const accessToken = result.data.match(/'accessToken'\, '\w+-\w+-\w+-\w+-\w+'/)[0].split('\'')[3]
+        console.log(accessToken)
+
+
+        let joinConfig = {
+            url: `/user-api/course/join?accessToken=${accessToken}`,
+            method: "POST",
+            data: {
+                course: courseId,
+                subOrg: '181042Y1',
+                nid: 'N000100250006',
+                cardNo:value
+            }
+        }
+
+        const delay  = Math.random()*60
+        setTimeout(async ()=>{
+            const result3 = await requset(joinConfig)
+            console.log(result3.data.result.cardNo)
+        },1000 * delay)
+        
+    }
+}
+
+async function getYouthStudyAnswer(){
+    let getANswerConfig = {
+        url : 'http://h5.cyol.com/special/daxuexi/edxasojtbc/css/index.css'
+    }
+    const result = await axios(getANswerConfig)
+    
+    const e = /.s[5-8]{\w{}/
+    
+    // console.log(e.test(result.data))
+}
+
+getYouthStudyAnswer()
+youthStudyRepoter()
+
